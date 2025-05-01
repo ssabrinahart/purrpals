702e439d96dccc4ebe17de38b5fb7c12c8e8654a
ssabrinahart <sabrinahart@vt.edu>
2025-04-30 22:17:08 -0400

    fixed files

--- purrpal/startup.sh
+++ purrpal/startup.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-python manage.py collectstatic && gunicorn --workers 2 myproject.wsgi
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
+gunicorn --workers 2 purrpal.wsgi:application --bind 0.0.0.0:8000
+
